a53698144b670a8e65316ac2f05cdee489c96bff
Remy Grimmer <remygrimmer@gmail.com>
2014-11-04 17:14:07 +0100

    Added client map visitor

---
+++ src/include/gzzzt/client/ClientMapVisitor.h
@@ -0,0 +1,43 @@
+/*
+ * Gzzzt, a Bomberman clone with robots and lightnings!
+ * Copyright (C) 2014 Gzzzt team (see AUTHORS)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef GZZZT_CLIENT_MAP_VISITOR_H
+#define GZZZT_CLIENT_MAP_VISITOR_H
+
+#include <vector>
+#include <tmx/LayerVisitor.h>
+
+namespace gzzzt {
+
+    class ClientMapVisitor : public tmx::LayerVisitor {
+    public:
+        
+        ClientMapVisitor() {};
+        ClientMapVisitor(std::vector<unsigned int>* GID);
+        
+        virtual void visitTileLayer(tmx::TileLayer& layer) override;
+        virtual void visitObjectLayer(tmx::ObjectLayer& layer) override;
+        virtual void visitImageLayer(tmx::ImageLayer& layer) override;
+        
+    private:
+        std::vector<unsigned int>* m_GID;
+    };
+
+}
+
+
+#endif // GZZZT_CLIENT_MAP_VISITOR_H
